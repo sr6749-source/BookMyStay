763d37f81a7622d2e745431fe1962eba68aa020e
sr6749-source <sr6749@srmist.edu.in>
2026-04-08 08:49:26 +0530

    UC1 completed by 11349

---
+++ UC/src/BookMyStay.java
@@ -0,0 +1,7 @@
+public class BookMyStay {
+    public static void main(String[] args){
+        System.out.println("Welcome to the hotel booking management system");
+        System.out.println("system initialized Ver 1.00");
+    }
+
+}
